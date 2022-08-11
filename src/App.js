9921a6e5a74d16a231a3351a61788229d338c611
Roland <cook.rolandcook.roland@gmail.com>
2022-08-11 01:21:10 -0400

    hero section
    part of hero section done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import Navbar from "./components/Navbar";
+import Herosection from "./components/Herosection"
 
 function App() {
   return (
+    <div>
     <Navbar/>
+
+    <div className="page-sections">
+    <Herosection/>
+   
+    </div>
+
+    </div>
   ) ;
 }
 
--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "../styles/heroSection.css"
 
 
 const Herosection = ()=>{
     return (
-        <div></div>
+        
+        <section >
+            
+            <div className="about-me">
+                <h3>Hi, My name is</h3>
+                <h1 className="name">Roland Cook</h1>
+                <h3>I build projects that help people</h3>
+            </div>
+            <div>RANDOM TEXT</div>
+        </section>
     )
 }
 
