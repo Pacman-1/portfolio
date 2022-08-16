6c9ea4b65678995f8fb1eaae0af09be5aec2b82e
Roland <cook.rolandcook.roland@gmail.com>
2022-08-16 10:12:32 -0400

    About me section done

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -6,10 +6,11 @@ const Aboutme = ()=>{
 return(
     <section className="section-container">
         <div className="about-me-section">
+            <span></span>
             <h1 className="about-me-text">About Me</h1>
 
-            <p>Hi, my name is Roland</p>
-            <p>I primarily work with CSS, HTML ,Javascript and React. </p>
+            <p>Hi, my name is Roland Cook, a Front End Developer from Concord, NC. I love building websites that solve life's unique problems.</p>
+            <p>I primarily work with CSS, HTML ,Javascript and React. I have build a number of websites and applications using these skills. I also enjoy working in Photoshop</p>
         </div>
         <img src={imagePhoto} alt="Logo" />
     </section>
