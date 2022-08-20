ceaeecb4ab7225bd48d601a8f985636716d2f1a2
Roland <cook.rolandcook.roland@gmail.com>
2022-08-20 14:12:40 -0400

    added technology section
    need to set up contact me section

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,11 +1,22 @@
 import React from "react";
-
+import "../styles/technologies.css"
 
 const Technologies = ()=>{
 return (
     <div className="technology-section">
-       jnv
-       <span></span>
+
+    <div className="technology-head">
+    <span></span>
+    <h1>Technologies</h1>
+    </div>
+     
+     <div className="tech-list">
+            <li><i class="fa-brands fa-html5"></i></li>
+            <li><i class="fa-brands fa-css3-alt"></i></li>
+            <li><i class="fa-brands fa-square-js"></i></li>
+            <li><i class="fa-brands fa-react"></i></li>
+     </div>
+     
            
     </div>
 )
